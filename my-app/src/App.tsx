6389f6c05296c9873e04eea4616a9dfcb7b5e554
Hemanth <hemanthpenmatsa@gmail.com>
2025-05-02 15:11:46 -0700

    add React basic app

---
+++ my-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Dashboard1 from './components/Dashboard1';
+import DashboardHeader from './components/DashboardHeader';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import StatCard from './components/StatCard';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <Dashboard1 />
+    </div>
+  );
+}
+
+export default App;
