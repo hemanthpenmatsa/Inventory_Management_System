3e4e2320c926785dc21a63b49c4ecf656f79ae62
Hemanth <hemanthpenmatsa@gmail.com>
2025-05-02 15:56:40 -0700

    Added tailwind css to package.json

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import Dashboard1 from './components/Dashboard1';
-import DashboardHeader from './components/DashboardHeader';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import StatCard from './components/StatCard';
+import './index.css';
 import './App.css';
 
 function App() {
